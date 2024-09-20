539d2344ae7b412848d35ff4ba76b6c9ed53f97d
Stanley Otieno <83674953+Stanley17932@users.noreply.github.com>
2024-09-20 20:42:51 +0300

    add

--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/Stanley17932/microservice-with-golang/application"
 )
 
 func main() {
 	app := application.New()
+
+	signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
 
 	err := app.Start(context.TODO())
 
