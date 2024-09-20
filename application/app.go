fe499a01a3c4cc6da9988535d71210052314c7f8
Stanley Otieno <83674953+Stanley17932@users.noreply.github.com>
2024-09-20 21:02:43 +0300

    add

--- application/app.go
+++ application/app.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -31,6 +32,13 @@ func (a *App) Start(ctx context.Context) error {
 	if err != nil {
 		return fmt.Errorf("failed to connect to redis: %w", err)
 	}
+
+	defer func() {
+		if err := a.rdb.Close(); err != nil {
+			fmt.Println("failed to close redis", err)
+		}
+	}()
+
 	fmt.Println("Starting server")
 
 	ch := make(chan error, 1)
@@ -38,8 +46,19 @@ func (a *App) Start(ctx context.Context) error {
 	go func() {
 		err = server.ListenAndServe()
 		if err != nil {
-			return fmt.Errorf("failed to start server: %w", err)
+			ch <- fmt.Errorf("failed to start server: %w", err)
 		}
+		close(ch)
 	}()
+
+	select {
+	case err = <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+		return server.Shutdown(timeout)
+	}
+
 	return nil
 }
