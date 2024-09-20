be0f392b147fc0503b1f2517e6f7bacf4e49d7c7
Stanley Otieno <83674953+Stanley17932@users.noreply.github.com>
2024-09-20 21:53:10 +0300

    add

---
+++ repository/order/redis.go
@@ -0,0 +1,114 @@
+package order
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/Stanley17932/microservice-with-golang/model"
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisRepo struct {
+	Client *redis.Client
+}
+
+func orderIDKey(id uint64) string {
+	return fmt.Sprintf("order:%d", id)
+}
+
+func (r *RedisRepo) Insert(ctx context.Context, order model.Order) error {
+
+	data, err := json.Marshal(order)
+	if err != nil {
+		return fmt.Errorf("failed to encode order:%m", err)
+	}
+
+	key := orderIDKey(order.OrderID)
+
+	txn := r.Client.TxPipeline()
+
+	res := r.Client.SetNX(ctx, key, string(data), 0)
+	if err := res.Err(); err != nil {
+		txn.Discard()
+		return fmt.Errorf("failed to set: %m", err)
+	}
+	if err := r.Client.SAdd(ctx, "orders", key).Err(); err != nil{
+		txn.Discard()
+		return fmt.Errorf("failed to add to orders set: %w", err)
+	}
+	if _, err := txn.Exec(ctx); err != nil{
+		return fmt.Errorf("failed to exec: %w", err)
+	}
+	return nil
+}
+
+var ErrNotExist = errors.New("order does not exist")
+
+func (r *RedisRepo) FindByID(ctx context.Context, id uint64) (model.Order, error) {
+	key := orderIDKey(id)
+
+	value, err := r.Client.Get(ctx, key).Result()
+	if errors.Is(err, redis.Nil) {
+		return model.Order{}, ErrNotExist
+	} else if err != nil {
+		return model.Order{}, fmt.Errorf("get order:%m", err)
+	}
+	var order model.Order
+	err = json.Unmarshal([]byte(value), &order)
+
+	if err != nil {
+		return model.Order{}, fmt.Errorf("failed to decode order json: %m", err)
+	}
+	return order, nil
+}
+
+func (r *RedisRepo) DeleteByID(ctx context.Context, id uint64) error {
+	key := orderIDKey(id)
+
+	txn := r.Client.Del(ctx, key).Err()
+
+	err := r.Client.Del(ctx, key).Err()
+	if errors.Is(err, redis.Nil) {
+		txn.Discard()
+		return ErrNotExist
+	} else if err != nil {
+		txn.Discard()
+		return fmt.Errorf("get order:%m", err)
+	}
+
+	if err := txn.SRem(ctx, "orders", key).Err(); err != nil{
+		txn.Discard()
+		return fmt.Errorf("failed to remvoe from orders set: %m", err)
+	}
+	if _, err := txn.Exec(ctx); err != nil {
+		return fmt.Errorf("failed to exec: %w", &err)
+	}
+	return nil
+}
+func (r *RedisRepo) Update(ctx context.Context, order model.Order) error {
+	data, err := json.Marshal(order)
+	if err != nil {
+		return fmt.Errorf("failed to encode order:%m", err)
+	}
+	key := orderIDKey(order.OrderID)
+
+	err = r.Client.SetXX(ctx, key, string(data), 0).Err()
+	if errors.Is(err, redis.Nil) {
+		return ErrNotExist
+	} else if err != nil {
+		return fmt.Errorf("set order: %m", err)
+	}
+	return nil
+}
+
+type FindAllPage struct {
+	Size   uint
+	Offset uint
+}
+
+func (r *RedisRepo) FindAll(ctx context.Context) ([]model.Order, error) {
+	retunrn nil, nil
+}
